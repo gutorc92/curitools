66aad3daebad33db01c928d4f14d097dc41d814d
Gustavo Coelho <gutorc@hotmail.com>
2017-09-13 23:47:25 -0300

    Test of submissions table
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- curitools/status.py
+++ curitools/status.py
@@ -7,11 +7,6 @@ import os
 import subprocess
 from clint.textui import colored
 
-def read_input(file_name):
-    fn = open(file_name, 'r')
-    text = fn.read()
-    fn.close()
-    return text
 
 class SubmissionStatusOutput(object):
     
@@ -32,21 +27,28 @@ class SubmissionStatusOutput(object):
         elif "Compilation" in text:
             text = colored.yellow(text)
         return text
-        
+       
     def print_table(self):
+        for lines in self.table:
+            for column in lines:
+                print(column,'|', end=" ", flush=True)
+            print("")
+
+    def extract_table(self):
         self.separar_quadro()
+        lines = []
         for line in self.table.findAll('tr'):
+            line_list = []
             for l in line.findAll('td'):
                 if l.find('sup'):
                     l.find('sup').extract()
-                print(self.format_result(l.getText().replace("\n","").strip()),'|', end=" ", flush=True)
-            print("")
+                line_list.append(l.getText().replace("\n","").strip())
+                #print(self.format_result(l.getText().replace("\n","").strip()),'|', end=" ", flush=True)
+            #print("")
+            lines.append(line_list)
+        self.table = lines
         
-def main():
-    text = read_input("/home/gustavo/Documents/uritools/test/tabelasubmissoes.html")
-    s = SubStatus(text)
-    s.print_table()
-    
+  
 if __name__ == "__main__":
     main()           
 
---
+++ curitools/tests/test_status.py
@@ -0,0 +1,21 @@
+import unittest
+from curitools.status import SubmissionStatusOutput  
+import os
+
+class TestSubmissionsPage(unittest.TestCase):
+
+
+    def test_extract_table(self):
+        repositorio_dir = os.path.dirname(os.path.realpath(__file__))
+        file_name = os.path.join(repositorio_dir, "tabelasubmissoes.html")
+        fn = open(file_name, 'r')
+        text = fn.read()
+        fn.close()
+        s = SubmissionStatusOutput(text)
+        s.extract_table()
+        s.print_table()
+        self.assertEqual(len(s.table), 22)
+
+    
+if __name__ == '__main__':
+    unittest.main()
