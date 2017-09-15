2216b35d259e252a6c5fbffd3881a9e4bc871c34
Gustavo Coelho <gutorc@hotmail.com>
2017-09-15 00:40:37 -0300

    Setup problem
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- curitools/setup_problems.py
+++ curitools/setup_problems.py
@@ -11,40 +11,43 @@ class SetupProblem(object):
         self.workdir = workdir
         self.dir_template = dir_template
         self.language = language
+        self.list_files =  { 'c++' : { 'base' : 'base.cpp', 'makefile' : 'Makefile.c++'},
+                             'python' : {'base' : "base.py" }}
 
     def has_makefile(self):
-        if "c++" in self.language:
+        if self.language in self.list_files and "makefile" in self.list_files[self.language]:
             return True
         return False
 
     def get_base_template(self):
-        if "c++" in self.language:
-            base_file = os.path.join(self.dir_template, "base.cpp") 
-        else:
-            base_file = os.path.join(self.dir_template, "base.cpp") 
-
-        return base_file
+        return os.path.join(self.dir_template, self.list_files[self.language]["base"]) 
 
     def get_makefile_template(self):
-        if "c++" in self.language:
-            make_file =  os.path.join(self.dir_template, "Makefile.c++")      
-        else:
-            make_file =  os.path.join(self.dir_template, "Makefile.c++")      
-
-        return  make_file
+        return os.path.join(self.dir_template, self.list_files[self.language]["makefile"]) 
          
     def create_files(self):
+        print(self.workdir)
         if os.path.isdir(self.workdir):
             problem_dir = os.path.join(self.workdir, self.problem)
+            print("Aqui")
             if not os.path.isdir(problem_dir):
                 os.makedirs(problem_dir)
-                base_file = self.get_base_template()
-                filename, file_extension = os.path.splitext(base_file)
-                problem_file = ".".join([self.problem, file_extension])
-                problem_file =  os.path.join(self.workdir, self.problem, problem_file)  
-                copy(base_file, problem_file)
-                if self.has_makefile():
-                    self.create_makefile(problem_dir)
+                self.create_base_files(problem_dir)
+            else:
+                raise FileExistsError("Pasta com o nome do problema ja existe no diretorio")
+        else:
+            raise NotADirectoryError("Tentando criar arquivos para resolver o problema em um lugar que nao e um diretorio") 
+
+    def create_base_files(self, problem_dir):
+        base_file = self.get_base_template()
+        if not os.path.exists(base_file):
+            raise FileExistsError("O arquivo de template nao existe.")
+        filename, file_extension = os.path.splitext(base_file)
+        problem_file = "".join([self.problem, file_extension])
+        problem_file =  os.path.join(problem_dir, problem_file)  
+        copy(base_file, problem_file)
+        if self.has_makefile():
+            self.create_makefile(problem_dir)
 
     def create_makefile(self, problem_dir):
         make_file = self.get_makefile_template()
--- curitools/tests/test_setupproblems.py
+++ curitools/tests/test_setupproblems.py
@@ -1,6 +1,7 @@
 import unittest
 from curitools.setup_problems import SetupProblem 
 import os
+from shutil import rmtree
 
 class TestSetupProblem(unittest.TestCase):
 
@@ -10,11 +11,15 @@ class TestSetupProblem(unittest.TestCase):
         template_dir = os.path.dirname(os.path.realpath(__file__))
         self.test_dir = os.path.dirname(os.path.realpath(__file__))
         self.template_dir = os.path.join(template_dir,"..", "templates")
-        print(self.template_dir)
-        self.setup = SetupProblem("1010", cwd, self.template_dir, "c++")
+        self.setup = SetupProblem("1006", cwd, self.template_dir, "c++")
          
     def test_hasmakefiles(self):
         self.assertTrue(self.setup.has_makefile())
+        self.setup = SetupProblem("1010", os.getcwd(), self.template_dir, "python")
+        self.assertFalse(self.setup.has_makefile())
+        self.setup = SetupProblem("1010", os.getcwd(), self.template_dir, "c")
+        self.assertFalse(self.setup.has_makefile())
+        
 
     def test_get_base_template(self):
         base_file = os.path.join(self.template_dir, "base.cpp")
@@ -28,7 +33,23 @@ class TestSetupProblem(unittest.TestCase):
         self.setup.create_makefile(self.test_dir)
         self.assertTrue(os.path.exists(os.path.join(self.test_dir,"Makefile"))) 
         os.remove(os.path.join(self.test_dir,"Makefile"))
-        
+
+    def test_create_files(self):
+        self.setup = SetupProblem("1006", self.test_dir, self.template_dir, "c++")
+        self.setup.create_files()
+        self.assertTrue(os.path.exists(os.path.join(self.test_dir,"1006"))) 
+        self.assertTrue(os.path.exists(os.path.join(self.test_dir,"1006", "1006.cpp"))) 
+        self.assertTrue(os.path.exists(os.path.join(self.test_dir,"1006", "Makefile"))) 
+        rmtree(os.path.join(self.test_dir,"1006"))
+        self.setup = SetupProblem("1010", os.path.join(self.test_dir, "test_settings.py"), self.template_dir, "c++")
+        self.assertRaises(NotADirectoryError, self.setup.create_files())
+
+    def test_create_base_files(self):
+        self.setup.create_base_files(self.test_dir)
+        self.assertTrue(os.path.exists(os.path.join(self.test_dir,"1006.cpp"))) 
+        os.remove(os.path.join(self.test_dir,"1006.cpp"))   
+        self.setup = SetupProblem("1010", os.getcwd(), os.path.join(self.template_dir, ".."), "c++")
+        #self.assertRaises(FileExistsError, self.setup.create_base_files(self.test_dir))
 
 if __name__ == '__main__':
     unittest.main()
