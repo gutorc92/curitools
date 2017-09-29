d2246542f889193a01952d4a19f9e97f4f2eb83d
Gustavo Rodrigues Coelho <B15599226@ipea.gov.br>
2017-09-29 15:35:13 -0300

    Settings can have multiple attributes
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- curitools/settings.py
+++ curitools/settings.py
@@ -7,10 +7,14 @@ import getpass
 class MissingFileSettings(Exception):
     pass
 
+class MissingValueRequired(Exception):
+    pass
+
 class Settings(object):
     
     def __init__(self, file_path  = None):
         self.file_path = file_path if file_path is not None else self.find_settings_file()
+        self.settings_values = {"user": "required", "password" : "required", "language" : "notrequired"}
 
     def find_settings_file(self):
         uritools_dir = os.path.expanduser("~")
@@ -55,38 +59,34 @@ class Settings(object):
         else:
             print("O arquivo de configuracao e necessario")
             return False
-            
-    def get_user(self,line):
-        m = re.search("user: (.*)", line)
-        if m is not None:
-            return m.group(1)
-        return m
 
-    def get_password(self, line):
-        m = re.search("password: (.*)", line)
+    def get_setting(self, setting, line):
+        regex_text = setting + ": (.*)"
+        m = re.search(regex_text, line)
         if m is not None:
             return m.group(1)
-        return m
+        return None
 
     def read_settings(self):
-        user = ""
-        password = ""
-        if self.file_path is None:
+        if self.file_path is None or not os.path.isfile(self.file_path):
              raise MissingFileSettings("O arquivo de configuracao nao foi encontrado")
         with open(self.file_path, "r") as handle:
-            for line in handle:
-                if self.get_user(line) is not None:
-                    user = self.get_user(line)
-                if self.get_password(line) is not None:
-                    password = self.get_password(line)
-        self.user = user
-        self.password = password
-        return user,password
+            text = handle.read()
+        return text
+
+    def extract_settings(self):
+        text = self.read_settings()
+        for setting, value in self.settings_values.items():
+            found = self.get_setting(setting, text)
+            if value == "required" and found == None:
+                raise MissingValueRequired("Setting %s not found on file %s" % (setting, self.file_path))
+            self.__dict__[setting] = found
 
     def get_settings(self):
         if hasattr(self, 'user') and hasattr(self, 'password') and self.user and self.password:
             return self.user, self.password
         else:
-            return self.read_settings()
+            self.extract_settings()
+            return self.get_settings()
 
 
--- curitools/tests/test_settings.py
+++ curitools/tests/test_settings.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from curitools.settings import Settings
+from curitools.settings import Settings, MissingValueRequired
 
 class TestSettings(unittest.TestCase):
 
@@ -22,9 +22,34 @@ class TestSettings(unittest.TestCase):
         file_created.write("password: testando")
         file_created.close()
         s = Settings()
-        s.read_settings()
+        s.get_settings()
         assert "teste@hotmail.com" == s.user
         assert "testando" == s.password
+        os.remove(file_settings)
+
+    def test_execption_setting_required(self):
+        uritools_dir = os.path.expanduser("~")
+        file_settings = os.path.join(uritools_dir, ".uri.settings")
+        file_created = open(file_settings, "w")
+        file_created.write("user: teste@hotmail.com\n")
+        file_created.close()
+        s = Settings()
+        self.assertRaises(MissingValueRequired, s.get_settings)
+        os.remove(file_settings)
+
+    def test_get_user_and_password_and_language(self):
+        uritools_dir = os.path.expanduser("~")
+        file_settings = os.path.join(uritools_dir, ".uri.settings")
+        file_created = open(file_settings, "w")
+        file_created.write("user: teste@hotmail.com\n")
+        file_created.write("password: testando\n")
+        file_created.write("language: c++")
+        file_created.close()
+        s = Settings()
+        s.get_settings()
+        assert "teste@hotmail.com" == s.user
+        assert "testando" == s.password
+        assert "c++" == s.language
         os.remove(file_settings)
     
 if __name__ == '__main__':
