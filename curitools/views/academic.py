3c6d70454a4c73eb8b9a276ed5c471594fe6aef9
Gustavo Rodrigues Coelho <gutorc@hotmail.com>
2017-09-29 17:48:37 -0300

    BaseView
    Signed-off-by: Gustavo Rodrigues Coelho <gutorc@hotmail.com>

--- curitools/views/academic.py
+++ curitools/views/academic.py
@@ -4,18 +4,13 @@ from bs4.element import NavigableString
 import re
 import sys
 import os
+from curitools.views.base_view import BaseView
 
-
-class AcademicView(object):
+class AcademicView(BaseView):
     
     def __init__(self, page):
-        self.page = page
+        super(AcademicView, self).__init__(page)
 
-    def content(self):
-        soup = BeautifulSoup(self.page, 'html.parser')
-        self.content_html = soup.find("ul", {"id": "groups"})
-        return self.content_html
-        
     def get_name(self, element):
         name_element = element.find("h3")
         return name_element.getText() if name_element is not None else None
@@ -25,7 +20,7 @@ class AcademicView(object):
         return prof_element.getText() if prof_element is not None else None
 
     def extract_table(self):
-        cont = self.content()
+        cont = self.content("ul", "groups")
         list_html = cont.findAll("li")
         self.data = []
         for e in list_html:
@@ -34,26 +29,7 @@ class AcademicView(object):
             if name is not None and prof is not None:
                 self.data.append([name, prof])
 
-    def get_max_length(self):
-        max_column = []
-        for lines in self.data:
-            for i in range(0, len(lines)):
-                try:
-                    if max_column[i] < len(lines[i]):
-                        max_column[i] = len(lines[i])
-                except IndexError:
-                    max_column.insert(i,len(lines[i]))
-        self.max_column = max_column
-
-    def print_table(self):
-        self.extract_table()
-        self.get_max_length()
-        for lines in self.data:
-            for i in range(0, len(lines)):
-                text = "{0:{width}}".format(lines[i], width = self.max_column[i])
-                print(text,'|', end=" ", flush=True)
-            print("")
-
+  
 
   
 
---
+++ curitools/views/base_view.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin/python3.5
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString
+import re
+import sys
+import os
+
+
+class BaseView(object):
+    
+    def __init__(self, page):
+        self.page = page
+
+    def content(self, main_content_type, id_content):
+        soup = BeautifulSoup(self.page, 'html.parser')
+        self.content_html = soup.find(main_content_type, {"id": id_content})
+        return self.content_html
+        
+
+
+    def extract_table(self):
+        pass
+
+    def get_max_length(self):
+        max_column = []
+        for lines in self.data:
+            for i in range(0, len(lines)):
+                try:
+                    if max_column[i] < len(lines[i]):
+                        max_column[i] = len(lines[i])
+                except IndexError:
+                    max_column.insert(i, len(lines[i]))
+        self.max_column = max_column
+
+    def print_table(self):
+        self.extract_table()
+        self.get_max_length()
+        for lines in self.data:
+            for i in range(0, len(lines)):
+                text = "{0:{width}}".format(lines[i], width = self.max_column[i])
+                print(text,'|', end=" ", flush=True)
+            print("")
+
+
+  
+
+
+
